503ff944fc124321d6786928da855fec10d2aed6
juice wrld <ankaraunifedaisi@gmail.com>
2023-09-15 16:58:17 +0300

    include data file. this will used later

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
+import userData from "./data.json"
 
 function App() {
 
   return (
     <>
+    <div>
+      <h1>Hello</h1>
+      {userData.map(e => (
+        <p key={e.id}>{e.name}</p>
+      ))}
+    </div>
     </>
   )
 }
