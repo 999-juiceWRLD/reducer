4e159f233d1ed781a8935f5a026ad26f4ee171c3
juice wrld <ankaraunifedaisi@gmail.com>
2023-09-16 01:54:29 +0300

    styling classes final touches

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import './App.css'
 import { Counter } from './Counter'
 import userData from "./data.json"
+import { useReducer } from 'react'
 
 function App() {
+
+  const reducerFunc = (users, action) => {
+    const userDataDiv = document.querySelector('div.user-data')
+    const showButton = document.getElementById('show-button')
+    switch (action.type) {
+      case 'HIDE':
+        userDataDiv.classList.add('user-data-hide')
+        showButton.style.display = 'block'
+        return []
+      case 'SHOW':
+        userDataDiv.classList.remove('user-data-hide')
+        showButton.style.display = 'none'
+        users = userData
+        return users
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducerFunc, userData)
 
   return (
     <>
@@ -10,11 +29,13 @@ function App() {
       <h1>Hello</h1>
       <div className='counter-user'>
         <Counter />
-        <div>
-          {userData.map(e => (
+        <div className='user-data'>
+          {state.map(e => (
             <p key={e.id}>{e.name}</p>
           ))}
+          <button onClick={() => { dispatch({ type: 'HIDE' }) }}>click to remove</button>
         </div>
+          <button id='show-button' style={{ display: 'none' }} onClick={() => { dispatch({ type: 'SHOW' }) }}>click to show</button>
       </div>
     </div>
     </>
